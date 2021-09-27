41017f33861cf61cce22c79d0278fe0745bd1e32
wangteng <1638235292@qq.com>
2021-09-27 12:17:31 +0800

    增加推理功能

--- src/main/java/com/hitices/basicenvstatusinfer/client/C45Client.java
+++ src/main/java/com/hitices/basicenvstatusinfer/client/C45Client.java
@@ -4,27 +4,54 @@ import com.hitices.basicenvstatusinfer.core.C45Core;
 import com.hitices.basicenvstatusinfer.model.AttributeNode;
 import com.hitices.basicenvstatusinfer.utils.C45Utils;
 import com.hitices.basicenvstatusinfer.utils.DecisionTreeUtils;
+import lombok.Getter;
+import org.apache.commons.lang3.math.NumberUtils;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 public class C45Client {
 
+    private static C45Client c45Client = null;
+
+    @Getter
+    private AttributeNode rootNode = null;
+
+    @Getter
+    private C45Core core = null;
+
+    public static C45Client getC45Client(String file) throws IOException {
+        if(c45Client == null){
+            c45Client = new C45Client();
+            c45Client.start();
+        }
+        return c45Client;
+    }
+
     public static void main(String[] args) throws IOException {
-        new C45Client().start();
+        C45Client.getC45Client("file");
+//        List<String> featureList = new ArrayList<>(Arrays.asList("OutLook","Humidity","PlayTennis"));
+//        List<String> dataList = new ArrayList<>(Arrays.asList("Sunny","80"));
+//        String result = C45Client.getC45Client("file").getAnswer(featureList,dataList);
+//        System.out.println(result);
+
     }
     
     private void start() throws IOException {
-        List<List<String>> rawData = DecisionTreeUtils.getTrainingData("./data/PlayGolf.txt");
+        List<List<String>> rawData = DecisionTreeUtils.getTrainingData("./data/test.txt");
         C45Utils.transformContinuouslyVariables(rawData);
-        C45Core core = new C45Core();
+//        C45Core core = new C45Core();
+        core = new C45Core();
         createDecisionTree(core, rawData);
     }
     
     private void createDecisionTree(C45Core core, List<List<String>> currentData) {
         Map<String, Double> maxIGRatioMap = core.maxInformationGainRatio(currentData);
-        AttributeNode rootNode = new AttributeNode(maxIGRatioMap.keySet().iterator().next());
+//        AttributeNode rootNode = new AttributeNode(maxIGRatioMap.keySet().iterator().next());
+        rootNode = new AttributeNode(maxIGRatioMap.keySet().iterator().next());
         setAttributeNodeStatus(core, currentData, rootNode);
         DecisionTreeUtils.showDecisionTree(rootNode, "");
     }
@@ -81,4 +108,56 @@ public class C45Client {
         
         return attributeNode;
     }
+
+    /**
+     * 利用决策树预测结果
+     * @param featureList 特征列表
+     * @param  dataList 数据列表
+     *
+     * @return
+     */
+    private String getAnswer(List<String> featureList, List<String> dataList){
+        AttributeNode decisionNode = rootNode;
+        while (decisionNode != null){
+            if(decisionNode.isLeaf()){
+                return decisionNode.getAttributeName();
+            }
+            for (int i = 0; i<featureList.size();i++){
+                if(featureList.get(i).equals(decisionNode.getAttributeName())){
+                    String data = dataList.get(i);
+                    AttributeNode childNode = null;
+                    for (AttributeNode cn : decisionNode.getChildNodes()){
+                        if (NumberUtils.isCreatable(data)){
+                            // 连续型随机变量
+                            String s = cn.getParentStatus().substring(1);
+                            if(NumberUtils.isCreatable(s)){
+                                if(NumberUtils.toDouble(data) > NumberUtils.toDouble(s)){
+                                    childNode = cn;
+                                    break;
+                                }
+                            }else {
+                                if(NumberUtils.toDouble(data) <= NumberUtils.toDouble(s.substring(1))){
+                                    childNode = cn;
+                                    break;
+                                }
+                            }
+                        }else {
+                            // 离散型随机变量
+                            if (cn.getParentStatus().equals(data)){
+                                childNode = cn;
+                                break;
+                            }
+                        }
+                    }
+                    if (childNode == null){
+                        return "error";
+                    }
+                    decisionNode = childNode;
+                    break;
+                }
+            }
+        }
+        return "error";
+    }
+
 }
